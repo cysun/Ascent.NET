41a385131707ff39dc68edaad04fb9dc8f5d5295
Chengyu Sun <csun@calstatela.edu>
2023-02-26 10:34:03 -0800

    Implemented rubric assessment charts for course

--- Ascent/Areas/Rubric/Controllers/AssessmentController.cs
+++ Ascent/Areas/Rubric/Controllers/AssessmentController.cs
@@ -63,7 +63,6 @@ namespace Ascent.Areas.Rubric.Controllers
             ViewBag.Rubric = rubric;
             ViewBag.Course = course;
             ViewBag.Term = new Term(termCode);
-            ViewBag.Criteria = criteria;
 
             return View(dataTables);
         }
@@ -81,6 +80,72 @@ namespace Ascent.Areas.Rubric.Controllers
                     row[j + 1] = 0;
                 dataTable[i] = row;
             }
+            return dataTable;
+        }
+
+        public IActionResult Course(int courseId, int rubricId, int? fromYear, int? toYear)
+        {
+            var rubric = _rubricService.GetRubric(rubricId);
+            if (rubric == null) return NotFound();
+
+            var course = _courseService.GetCourse(courseId);
+            if (course == null) return NotFound();
+
+            var criteria = _rubricService.GetCriteria(rubricId);
+            if (criteria.Count == 0) return NotFound();
+
+            var criterionIdToIndex = new Dictionary<int, int>();
+            for (int i = 0; i < criteria.Count; ++i)
+                criterionIdToIndex[criteria[i].Id] = i;
+
+            var years = _rubricDataService.GetAssessmentYears(rubricId, courseId);
+            var _fromYear = fromYear ?? (years.Count >= 6 ? years[years.Count - 6] : years.First());
+            var _toYear = toYear ?? years.Last();
+
+            var dataTables = new Dictionary<RubricAssessmentType, object[][]>();
+            var dataByPerson = _rubricDataService.GetDataByPerson(rubricId, courseId, _fromYear, _toYear);
+            var selectedYears = years.Where(y => _fromYear <= y && y <= _toYear).ToList();
+            foreach (var dataEntry in dataByPerson)
+            {
+                var assessmentType = dataEntry.AssessmentType;
+                if (!dataTables.ContainsKey(assessmentType))
+                    dataTables[assessmentType] = createDataTable(selectedYears, criteria);
+
+                var yearIndex = selectedYears.IndexOf(dataEntry.Year);
+                var criterionIndex = criterionIdToIndex[dataEntry.CriterionId];
+                var ratingIndex = criteria[criterionIndex].ValueToRatingIndex(dataEntry.AvgRatingValue);
+                dataTables[assessmentType][yearIndex * criteria.Count + criterionIndex][ratingIndex + 2] =
+                    (int)dataTables[assessmentType][yearIndex * criteria.Count + criterionIndex][ratingIndex + 2] + 1;
+            }
+
+            ViewBag.Rubric = rubric;
+            ViewBag.Course = course;
+            ViewBag.Criteria = criteria;
+            ViewBag.FromYear = _fromYear;
+            ViewBag.ToYear = _toYear;
+            ViewBag.Years = years;
+
+            return View(dataTables);
+        }
+
+        // Each data table has years*criteria.Count rows. In each row, the first column is the year,
+        // the second column is the criterion name, followed by the count of each rating.
+        private object[][] createDataTable(List<int> years, List<RubricCriterion> criteria)
+        {
+            var dataTable = new object[years.Count * criteria.Count][];
+            for (int i = 0; i < years.Count; ++i)
+            {
+                for (int j = 0; j < criteria.Count; j++)
+                {
+                    var row = new object[criteria[j].Ratings.Count + 2];
+                    row[0] = years[i].ToString();
+                    row[1] = criteria[j].Name;
+                    for (int k = 0; k < criteria[j].Ratings.Count; k++)
+                        row[k + 2] = 0;
+                    dataTable[i * criteria.Count + j] = row;
+                }
+            }
+
             return dataTable;
         }
     }
--- Ascent/Services/RubricDataService.cs
+++ Ascent/Services/RubricDataService.cs
@@ -12,10 +12,23 @@ public class RubricDataService
         _db = db;
     }
 
+    // The PostgreSQL implementation of SqlQuery() is a bit odd. Instead of using the provided query directly, it
+    // uses the provided query as a subquery in "SELECT t.Value FROM <subquery> AS t". This means the result column
+    // of the query must be called "Value".
+    public List<int> GetAssessmentYears(int rubricId, int courseId) => _db.Database
+        .SqlQuery<int>($@"SELECT DISTINCT ""Year"" AS ""Value"" FROM ""RubricData""
+            WHERE ""RubricId"" = {rubricId} AND ""CourseId"" = {courseId}")
+        .OrderBy(y => y)
+        .ToList();
+
     public List<AssessmentSection> GetAssessmentSections(int rubricId) => _db.AssessmentSections
         .FromSql($@"SELECT DISTINCT ""CourseId"", ""Term_Code"" FROM ""RubricData"" WHERE ""RubricId"" = {rubricId}")
         .Include(s => s.Course)
         .ToList();
+
+    public List<RubricDataByPerson> GetDataByPerson(int rubricId, int courseId, int fromYear, int toYear) => _db.RubricDataByPerson
+    .Where(d => d.RubricId == rubricId && d.CourseId == courseId && fromYear <= d.Year && d.Year <= toYear)
+    .ToList();
 
     public List<RubricDataByPerson> GetDataByPerson(int rubricId, int courseId, int termCode) => _db.RubricDataByPerson
         .Where(d => d.RubricId == rubricId && d.CourseId == courseId && d.TermCode == termCode)
