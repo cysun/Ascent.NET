32bccc0d1a250af778567a452d76c1406871d954
Chengyu Sun <csun@calstatela.edu>
2023-04-14 11:08:20 -0700

    TinyMCE config changes and UI tweaks

--- Ascent/wwwroot/js/tinymce-options.js
+++ Ascent/wwwroot/js/tinymce-options.js
@@ -4,6 +4,8 @@ tinymceOptions.standard = (selector, height) => ({
     selector: selector,
     plugins: "autolink code image link lists table wordcount fullscreen",
     menubar: false,
+    convert_urls: false,
+    browser_spellcheck: true,
     toolbar: "styles fontfamily forecolor backcolor bullist numlist indent outdent hr link image table code fullscreen",
     height: height ? height : 350
 });
@@ -12,6 +14,8 @@ tinymceOptions.minimal = (selector, height) => ({
     selector: selector,
     plugins: "autolink code image link lists wordcount fullscreen",
     menubar: false,
+    convert_urls: false,
+    browser_spellcheck: true,
     toolbar: "bold italic underline bullist numlist link",
     height: height ? height : 200
 });
