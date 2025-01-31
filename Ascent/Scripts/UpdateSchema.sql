3d405b135b35173af0392335a42c771a29c4576a
Chengyu Sun <csun@calstatela.edu>
2025-01-31 12:00:39 -0800

    Fixed the Rubric Rating index bug

--- Ascent/Scripts/UpdateSchema.sql
+++ Ascent/Scripts/UpdateSchema.sql
@@ -1,4 +1,1 @@
-ALTER TABLE "Messages" DROP COLUMN "IsFailed";
-
-DELETE FROM "__EFMigrationsHistory";
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion") VALUES ('20240821203922_InitialSchema', '8.0.8');
+UPDATE "RubricRatings" SET "Index" = "Value" - 1;
