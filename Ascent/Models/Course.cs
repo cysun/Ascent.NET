2b48cfc5ad4c4894828a9cdaa853128726f73136
Chengyu Sun <csun@calstatela.edu>
2025-11-24 00:58:54 -0800

    Updated to .NET 10

--- Ascent/Models/Course.cs
+++ Ascent/Models/Course.cs
@@ -1,7 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
 
 namespace Ascent.Models;
 
